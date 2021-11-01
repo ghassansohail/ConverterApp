ef84429f21db159d5bed942ce06c6b759a2ccde9
Ghassan Sohail <Ghassansohail@gmail.com>
2021-11-01 22:23:33 +0300

    Adds new activity

--- app/src/main/java/com/ghassan/myapplication/MainActivity.java
+++ app/src/main/java/com/ghassan/myapplication/MainActivity.java
@@ -23,8 +23,6 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        requestWindowFeature(getWindow().FEATURE_NO_TITLE);
-        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
