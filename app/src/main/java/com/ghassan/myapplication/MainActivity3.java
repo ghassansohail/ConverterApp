db8c592ff188597dd33d13865f70270eb0379592
Ghassan Sohail <Ghassansohail@gmail.com>
2021-11-09 19:57:45 +0300

    Final App

--- app/src/main/java/com/ghassan/myapplication/MainActivity3.java
+++ app/src/main/java/com/ghassan/myapplication/MainActivity3.java
@@ -3,6 +3,7 @@ package com.ghassan.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -35,5 +36,17 @@ public class MainActivity3 extends AppCompatActivity {
     }
 
 
+    public void gitLog(View view) {
+        Intent intent= new Intent (MainActivity3.this,MainActivity7.class);
+        startActivity(intent);
+    }
 
+    public void gitProfile(View view) {
+        String url="https://github.com/ghassansohail/ConverterApp.git";
+
+            Uri webpage = Uri.parse(url);
+            Intent i = new Intent(Intent.ACTION_VIEW, webpage);
+            startActivity(i);
+
+    }
 }
